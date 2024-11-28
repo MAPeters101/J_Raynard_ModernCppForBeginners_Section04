a81fc21fa01e0819015f7a5d83ebbb951ec9009f
Mark Peters <mapeters101@earthlink.net>
2024-11-27 21:52:17 -0500

    Work on lesson051,

---
+++ lesson051.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int times_two(int x) {
+	cout << "Returning " << x * 2 << endl;
+	return x * 2;
+}
+
+int main() {
+	int a{ 4 }, b{ 0 };
+	b = times_two(a);
+	cout << a << " times 2 equals " << b << endl;
+	return 0;
+}
+
+
