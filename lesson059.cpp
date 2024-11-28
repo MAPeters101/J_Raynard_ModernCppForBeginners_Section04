f56da60499f4f153ae82f6e7d6c3729fa11f7546
Mark Peters <mapeters101@earthlink.net>
2024-11-28 10:38:46 -0500

    Work on lesson059.

---
+++ lesson059.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int main() {
+	string test{ "xyz" };
+	auto it = test.begin();
+	while (it != test.end()) {
+		cout << *it << endl;
+		++it;
+	}
+	cout << "\n\n";
+
+	for (auto it = test.begin(); it != test.end(); ++it) {
+		cout << *it << endl;
+	}
+	cout << "\n\n";
+
+	for (auto it : test)
+		cout << it << endl;
+	cout << "\n\n";
+
+	for (auto it = test.rbegin(); it != test.rend(); ++it) {
+		cout << *it << endl;
+	}
+	cout << "\n\n";
+
+	for (auto it = test.cbegin(); it != test.cend(); ++it) {
+		cout << *it << endl;
+	}
+	cout << "\n\n";
+
+	return 0;
+}
+
+*/
