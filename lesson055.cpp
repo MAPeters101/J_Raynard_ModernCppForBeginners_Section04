943dfc14db747165640192a2d5d9da8f7737bc46
Mark Peters <mapeters101@earthlink.net>
2024-11-27 23:03:07 -0500

    Work on lesson to 8:20.

---
+++ lesson055.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int x{ 11 };
+	if (x < 0 || x > 10)
+		cout << "I can't count up to " << x << " because I don't have enough fingers!\n\n";
+
+	string animal{ "cat" };
+	if (animal == "cat" || animal == "dog")
+		cout << "I would like to have a " << animal << " as a pet.\n\n";
+	else
+		cout << "I don't think a " << animal << " would be very practical!\n\n";
+
+	char c{ 'Y' };
+	if (c == 'y' || c == 'Y')
+		cout << "I'll take that as a yes!\n\n";
+
+	int y{ -4 };
+	const int SCREEN_WIDTH{ 1200 }, SCREEN_HEIGHT{ 80 };
+	if (x < 0 || y < 0 || x > SCREEN_WIDTH || y > SCREEN_HEIGHT)
+		cout << "Your character has gone off screen.\n\n";
+
+
+	return 0;
+}
+
+
