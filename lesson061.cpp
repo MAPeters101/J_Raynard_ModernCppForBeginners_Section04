4c8cc12502efc83ddb411b5407a6490b8bb56f61
Mark Peters <mapeters101@earthlink.net>
2024-11-28 11:14:38 -0500

    Work on lesson061.

---
+++ lesson061.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/*
+int main() {
+	vector<int> v1;
+	vector<int> v2{ 4, 2, 3, 5, 1 };
+	vector<int> v3(3, 5);
+
+	string hello{ 'H', 'e', 'l', 'l', 'o' };
+	string triple_x(3, 'x');
+
+	cout << v1.empty() << "\n";
+	cout << v2.size() << "\n";
+	cout << v3.size() << "\n";
+	cout << hello.size() << "\n";
+	cout << "\n\n";
+
+	for (auto it = v2.begin(); it != v2.end(); ++it) {
+		cout << *it << '\t';
+	}
+	cout << "\n\n";
+
+	for (auto it : v3) {
+		cout << it << '\t';
+	}
+
+	return 0;
+}
+
+*/
