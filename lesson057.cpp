4da35272ab25946ee14bf269c245d2cf82497c03
Mark Peters <mapeters101@earthlink.net>
2024-11-27 23:25:58 -0500

    Work on lesson057.

---
+++ lesson057.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int main() {
+	string hello{ "Hello" };
+
+	cout << hello.empty() << endl;
+
+	cout << hello.size() << endl;
+
+
+	size_t hello_size{ hello.size() };
+	cout << hello_size << endl;
+
+	return 0;
+}
+*/
