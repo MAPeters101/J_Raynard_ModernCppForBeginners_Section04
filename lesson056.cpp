63b4e710cc77419e9ee372d2436cbf5355c592ef
Mark Peters <mapeters101@earthlink.net>
2024-11-27 23:17:31 -0500

    Work on lesson056.

---
+++ lesson056.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+/*
+int main() {
+	int x{ 0 };
+	cin >> x;
+	int i{ 1 };
+	while (i <= 12) {
+		cout << i << " times " << x << " is " << i * x << endl;
+		++i;
+	}
+	cout << "\n\n";
+
+	for (int i = 1; i <= 12; ++i)
+		cout << i << " times " << x << " is " << i * x << endl;
+
+	return 0;
+}
+*/
