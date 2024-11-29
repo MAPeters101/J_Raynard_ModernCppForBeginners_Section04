559e73c926e8e78854901b732d8aa93ee0903d3a
Mark Peters <mapeters101@earthlink.net>
2024-11-29 17:22:29 -0500

    Work on lesson063 to 7:00.

--- lesson063.cpp
+++ lesson063.cpp
@@ -5,11 +5,17 @@ using namespace std;
 int main() {
 	string instr{ "" };
 	cin >> instr;
+	cout << "\n\n";
+
 	string outstr{ "* Hello, " + instr + " *"};
 	size_t outstr_len = outstr.size();
 	string stars(outstr_len, '*');
+	string spaces((outstr_len - 2), ' ');
+	string stars_spaces{ "*" + spaces + "*" };
 	cout << stars << endl;
+	cout << stars_spaces << endl;
 	cout << outstr << endl;
+	cout << stars_spaces << endl;
 	cout << stars << endl;
 
 
