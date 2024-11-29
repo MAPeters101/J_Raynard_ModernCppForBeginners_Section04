4d2780399d61f3e912d89d7fee38fe5d70c8969f
Mark Peters <mapeters101@earthlink.net>
2024-11-29 14:32:20 -0500

    Work on lesson062.

---
+++ lesson062.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+/*
+int main() {
+	vector<string> v;
+	string s;
+	cout << "Please type in some words, sepated by spaces" << endl;
+	while (cin >> s) {
+		v.push_back(s);
+	}
+
+	cout << "You entered the following words: " << endl;
+	for (auto it : v) {
+		cout << it << endl;
+	}
+
+	sort(v.begin(), v.end());
+	cout << "Your words in alphabetical order: " << endl;
+	for (auto it : v) {
+		cout << it << endl;
+	}
+
+
+	return 0;
+}
+*/
+
