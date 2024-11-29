f90003f117673368c110d598ce6c29fcc3593e64
Mark Peters <mapeters101@earthlink.net>
2024-11-29 17:14:53 -0500

    Work on lesson063 to 5:10.

--- lesson063.cpp
+++ lesson063.cpp
@@ -5,9 +5,10 @@ using namespace std;
 int main() {
 	string instr{ "" };
 	cin >> instr;
-	string outstr{ "Hello, " + instr };
+	string outstr{ "* Hello, " + instr + " *"};
 	size_t outstr_len = outstr.size();
 	string stars(outstr_len, '*');
+	cout << stars << endl;
 	cout << outstr << endl;
 	cout << stars << endl;
 
