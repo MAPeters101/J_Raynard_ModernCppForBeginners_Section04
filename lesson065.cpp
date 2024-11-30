14f386066949f3ae3e7825623159430ae9894b6c
Mark Peters <mapeters101@earthlink.net>
2024-11-29 23:05:31 -0500

    Work on lesson065 to 8:20.   First attempt.

---
+++ lesson065.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+/*
+int main() {
+	int change{ 0 };
+	cout << "Please enter the amount of change: ";
+	cin >> change;
+	int remaining{ change };
+	int ndollars = remaining / 100;
+	if (ndollars) {
+		cout << ndollars << " dollars" << endl;
+	}
+	remaining %= 100;
+	int nhalves = remaining / 50;
+	if (nhalves) {
+		cout << nhalves << " halves" << endl;
+	}
+	remaining %= 50;
+	int nquarters = remaining / 25;
+	if (nquarters) {
+		cout << nquarters << " quarters" << endl;
+	}
+	remaining %= 25;
+	int ndimes = remaining / 10;
+	if (ndimes) {
+		cout << ndimes << " dimes" << endl;
+	}
+	remaining %= 10;
+	int nnickles = remaining / 5;
+	if (nnickles) {
+		cout << nnickles << " nickles" << endl;
+	}
+	remaining %= 5;
+	int npennies = remaining;
+	if (npennies) {
+		cout << npennies << " pennies" << endl;
+	}
+
+
+
+	return 0;
+}
+*/
+
