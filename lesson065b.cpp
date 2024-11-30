db8abdcc001e92fb698b7a81b96f8151251a636b
Mark Peters <mapeters101@earthlink.net>
2024-11-29 23:45:00 -0500

    Complete lesson065b.

--- lesson065b.cpp
+++ lesson065b.cpp
@@ -2,10 +2,35 @@
 
 using namespace std;
 
+string number_to_text(int number) {
+	string text{ "" };
+	switch (number) {
+	case 0:
+		text = "zero";
+		break;
+	case 1:
+		text = "one";
+		break;
+	case 2:
+		text = "two";
+		break;
+	case 3:
+		text = "three";
+		break;
+	case 4:
+		text = "four";
+		break;
+	case 5:
+		text = "five";
+		break;
+	}
+	return text;
+}
+
 int calc_dollars(int change) {
 	int dollars{ change / 100 };
 	if (dollars) {
-		cout << dollars << ((dollars > 1) ? " dollars" : " dollar") << endl;
+		cout << number_to_text(dollars) << ((dollars > 1) ? " dollars" : " dollar") << endl;
 	}
 	return change % 100;
 }
@@ -13,7 +38,7 @@ int calc_dollars(int change) {
 int calc_halves(int change) {
 	int halves{ change / 50 };
 	if (halves) {
-		cout << halves << ((halves > 1) ? " halves" : " half") << endl;
+		cout << number_to_text(halves) << ((halves > 1) ? " halves" : " half") << endl;
 	}
 	return change % 50;
 }
@@ -21,7 +46,7 @@ int calc_halves(int change) {
 int calc_quarters(int change) {
 	int quarters{ change / 25 };
 	if (quarters) {
-		cout << quarters << ((quarters > 1) ? " quarters" : " quarter") << endl;
+		cout << number_to_text(quarters) << ((quarters > 1) ? " quarters" : " quarter") << endl;
 	}
 	return change % 25;
 }
@@ -29,7 +54,7 @@ int calc_quarters(int change) {
 int calc_dimes(int change) {
 	int dimes{ change / 10 };
 	if (dimes) {
-		cout << dimes << ((dimes > 1) ? " dimes" : " dime") << endl;
+		cout << number_to_text(dimes) << ((dimes > 1) ? " dimes" : " dime") << endl;
 	}
 	return change % 10;
 }
@@ -37,17 +62,17 @@ int calc_dimes(int change) {
 int calc_nickles(int change) {
 	int nickles{ change / 25 };
 	if (nickles) {
-		cout << nickles << ((nickles > 1) ? " nickles" : " nickle") << endl;
+		cout << number_to_text(nickles) << ((nickles > 1) ? " nickles" : " nickle") << endl;
 	}
 	return change % 5;
 }
 
-int calc_pennies(int change) {
+void calc_pennies(int change) {
 	int pennies{ change };
 	if (pennies) {
-		cout << pennies << ((pennies > 1) ? " pennies" : " penny") << endl;
+		cout << number_to_text(pennies) << ((pennies > 1) ? " pennies" : " penny") << endl;
 	}
-	return 0;
+	return;
 }
 
 int get_input() {
@@ -60,19 +85,24 @@ int get_input() {
 }
 
 
-int main() {
-	int remaining{ 0 };
-	remaining = get_input();
-
+void show_change_usa(int change) {
+	int remaining{ change };
 	remaining = calc_dollars(remaining);
 	remaining = calc_halves(remaining);
 	remaining = calc_quarters(remaining);
 	remaining = calc_dimes(remaining);
 	remaining = calc_nickles(remaining);
-	remaining = calc_pennies(remaining);
+	calc_pennies(remaining);
+	return;
+}
 
+
+/*
+int main() {
+	int remaining{ get_input() };
+	show_change_usa(remaining);
 
 	return 0;
 }
-
+*/
 
