03f92e526f22e6afb01898fa665f4f8f1a151d86
Mark Peters <mapeters101@earthlink.net>
2024-11-29 22:21:49 -0500

    Work on lesson064 to 8:30.

--- lesson064.cpp
+++ lesson064.cpp
@@ -1,26 +1,35 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
 	int grade{ 0 };
-	int count{ 0 };
 	double sum{ 0.0 };
+	vector<int> grades;
 	cout << "Please enter the grades (-1 to finish)\n";
 	while (cin >> grade && grade >= 0) {
 		if (grade > 100) {
 			cout << "Please enter a grade between 0 and 100." << endl;
 		}
 		else {
+			grades.push_back(grade);
 			sum += grade;
-			++count;
 		}
 	}
-	if (count == 0)
+	sort(grades.begin(), grades.end());
+	for (auto it : grades) {
+		cout << it << ", ";
+	}
+	cout << "\n";
+
+	if (grades.size() == 0) {
 		cout << "No valid grades entered." << endl;
+	}
 	else {
-		cout << count << " grades enterd." << endl;
-		cout << "Average grade is " << sum / count << endl;
+		cout << grades.size() << " grades enterd." << endl;
+		cout << "Average grade is " << sum / grades.size() << endl;
 	}
 	return 0;
 }
