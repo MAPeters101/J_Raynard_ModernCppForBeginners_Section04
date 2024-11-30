33211891eba66defbf2001e6ee893f72ca1c7198
Mark Peters <mapeters101@earthlink.net>
2024-11-29 21:23:23 -0500

    Work on lesson063 to 9:30.

--- lesson063.cpp
+++ lesson063.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -17,6 +18,26 @@ int main() {
 	cout << outstr << endl;
 	cout << stars_spaces << endl;
 	cout << stars << endl;
+	cout << "\n\n";
+
+	vector<string> lines;
+	size_t content_len = outstr.size();
+	size_t width = content_len;
+	string top(width, '*');
+	lines.push_back(top);
+	string top_blanks(content_len-4, ' ');
+	string below_top{ "* " + top_blanks + " *" };
+	lines.push_back(below_top);
+	string content{ outstr };
+	lines.push_back(content);
+	string lower_blanks(content_len-4, ' ');
+	string above_bottom{ "* " + lower_blanks + " *" };
+	lines.push_back(above_bottom);
+	string bottom(width, '*');
+	lines.push_back(bottom);
+	for (auto it : lines) {
+		cout << it << endl;
+	}
 
 
 	return 0;
